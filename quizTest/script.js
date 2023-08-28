bb788ffbdad0eb318282a65c154ad8e30e012f44
Corey Hulls <coreyhsgames@outlook.com>
2023-08-28 18:53:28 +1200

    Difficulty Functionality

--- quizTest/script.js
+++ quizTest/script.js
@@ -1,25 +1,51 @@
-const startBtn = document.getElementById('start-btn');
+// Control Buttons
 const quitBtn = document.getElementById('quit-btn');
 const nextBtn = document.getElementById('next-btn');
+const closeBtn = document.getElementById('close-btn');
+
+// Answer Buttons
+const answerBtns = document.getElementById('answer-btns');
+
+// Question Information
 const questionContainer = document.getElementById('question-container');
 const questionText = document.getElementById('question');
 const questionImg = document.getElementById('question-image');
 const questionInfo = document.getElementById('question-info');
-const scoreInfo = document.getElementById('score-info');
-const answerBtns = document.getElementById('answer-btns');
-const mainHeader = document.getElementById('main-header');
+const difficultyInfo = document.getElementById('difficulty-info');
 const fullImgView = document.getElementById('full-img-view');
 const fullImg = document.getElementById('full-img')
-const closeBtn = document.getElementById('close-btn');
+const scoreInfo = document.getElementById('score-info');
+const mainHeader = document.getElementById('main-header');
+const questionResult = document.getElementById('question-result');
 
-let shuffledQuestions, currentQuestionIndex = 0, score = 0;
+// Difficulty Buttons
+const difficultyContainer = document.getElementById('difficulty-btns')
+const easyBtn = document.getElementById('easy-btn');
+const mediumBtn = document.getElementById('medium-btn');
+const hardBtn = document.getElementById('hard-btn');
 
-startBtn.addEventListener('click', startGame);
+let shuffledQuestions, currentQuestionIndex = 0, score = 0, selectedDifficulty, questions;
+
+// Click Events
 nextBtn.addEventListener('click', nextQuestion);
 quitBtn.addEventListener('click', quitGame);
-
 questionImg.addEventListener('click', showFullImg);
 closeBtn.addEventListener('click', closeFullImg);
+
+easyBtn.addEventListener('click', function() {
+    selectedDifficulty = 'Easy';
+    startGame(selectedDifficulty);
+});
+
+mediumBtn.addEventListener('click', function() {
+    selectedDifficulty = 'Medium';
+    startGame(selectedDifficulty);
+});
+
+hardBtn.addEventListener('click', function() {
+    selectedDifficulty = 'Hard';
+    startGame(selectedDifficulty);
+});
 
 function showFullImg() {
     fullImgView.classList.remove('hide');
@@ -33,12 +59,25 @@ function setFullImg(imgSrc) {
     fullImg.src = imgSrc;
 }
 
-function startGame() {
+function startGame(selDifficulty) {
+    questions = null;
     score = 0;
+
     scoreInfo.innerText = `Score: ${score} / 0`;
-    startBtn.classList.add('hide');
+    difficultyContainer.classList.add('hide');
     mainHeader.classList.add('hide');
     quitBtn.classList.remove('hide');
+    
+    if (selDifficulty == 'Easy') {
+        questions = easyQuestions;
+    } else if (selDifficulty == 'Medium') {
+        questions = mediumQuestions;
+    } else if (selDifficulty == 'Hard') {
+        questions = hardQuestions;
+    }
+
+    difficultyInfo.innerText = selDifficulty;
+
     shuffledQuestions = questions.sort(() => Math.random() - 0.5);
     currentQuestionIndex = 0;
     questionContainer.classList.remove('hide');
@@ -46,10 +85,11 @@ function startGame() {
 }
 
 function quitGame() {
-    startBtn.classList.remove('hide');
+    difficultyContainer.classList.remove('hide');
     mainHeader.classList.remove('hide');
     quitBtn.classList.add('hide');
     questionContainer.classList.add('hide');
+    nextBtn.classList.add('hide');
 }
 
 function nextQuestion() {
@@ -63,6 +103,9 @@ function setNextQuestion() {
 }
 
 function showQuestion(question) {
+    questionResult.classList.add('hide');
+    clearStatusClass(questionResult);
+
     questionInfo.innerText = `Question ${currentQuestionIndex + 1} of ${shuffledQuestions.length}`
     questionText.innerText = question.question;
 
@@ -109,13 +152,17 @@ function selectAnswer(element) {
     });
     if (shuffledQuestions.length > currentQuestionIndex + 1) {
         nextBtn.classList.remove('hide');
-    } else {
-        startBtn.innerText = 'Finish';
-        startBtn.classList.remove('hide');
     }
+
+    questionResult.classList.remove('hide');
 
     if (correct) {
         score++;
+        questionResult.innerText = "Correct!";
+        setStatusClass(questionResult, true);
+    } else {
+        questionResult.innerText = "Wrong!";
+        setStatusClass(questionResult, false);
     }
 
     scoreInfo.innerText = `Score: ${score} / ${currentQuestionIndex + 1}`;
@@ -136,7 +183,7 @@ function clearStatusClass(element) {
 }
 
 // Quiz Questions
-const questions = [
+const easyQuestions = [
     {
         type: 'word',
         question: 'What year was the Treaty of Waitangi signed?',
@@ -219,6 +266,74 @@ const questions = [
             { text: 'Christchurch', correct: false },
             { text: 'Wellington', correct: true },
             { text: 'Tauranga', correct: false }
+        ]
+    },
+    {
+        type: 'image',
+        question: "Name this native bird in New Zealand.",
+        image: "../images/quizTest/G615IPgjHL.jpg",
+        answers: [
+            { text: 'Emu', correct: false },
+            { text: 'Brown quail', correct: false },
+            { text: 'Rock pigeon', correct: false },
+            { text: 'Kiwi', correct: true }
+        ]
+    },
+    {
+        type: 'word',
+        question: "What is the name of New Zealand natives?",
+        answers: [
+            { text: 'Maori', correct: true },
+            { text: 'Polynesians', correct: false },
+            { text: 'Aboriginals', correct: false },
+            { text: 'Hawaiian', correct: true }
+        ]
+    },
+]
+
+const mediumQuestions = [
+    {
+        type: 'word',
+        question: "What is New Zealand's offical currency?",
+        answers: [
+            { text: 'New Zealand Dollar', correct: true },
+            { text: 'New Zealand Euro', correct: false },
+            { text: 'New Zealand Mark', correct: false },
+            { text: 'New Zealand Pound', correct: false }
+        ]
+    },
+]
+
+const hardQuestions = [
+    {
+        type: 'word',
+        question: "What year did the Sky Tower open?",
+        answers: [
+            { text: '1997', correct: true },
+            { text: '1995', correct: false },
+            { text: '2000', correct: false },
+            { text: '1990', correct: false }
+        ]
+    },
+    {
+        type: 'word',
+        question: "Which of these regions are part of the Realm of New Zealand?",
+        answers: [
+            { text: 'Hawaii', correct: false },
+            { text: 'Tokelau', correct: true },
+            { text: 'Solomon Islands', correct: false },
+            { text: 'Tuvalu', correct: false }
+        ]
+    },
+    {
+        type: 'image',
+        question: "What is the GDP Per Capita for New Zealand (2022 Estimate)?",
+        image: "../images/quizTest/ckCeon2YYJ.jpg",
+        answers: [
+            { text: '~$65,000', correct: false },
+            { text: '~$40,000', correct: false },
+            { text: '~$47,000', correct: true },
+            { text: '~$52,000', correct: false }
         ]
     },
 ]
